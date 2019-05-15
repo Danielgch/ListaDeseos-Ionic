a22fd459e31c158bdf60ec926ea1bfe9b28795f8
Daniel Giraldo <danielgiraldoch@gmail.com>
2019-05-15 10:14:14 -0500

    Lista terminados

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
 import { Lista } from 'src/app/models/lista.model';
@@ -10,6 +10,8 @@ import { Lista } from 'src/app/models/lista.model';
 })
 export class ListasComponent implements OnInit {
 
+  @Input() terminada = true;
+
   constructor(public deseosService: DeseosService,
     private router: Router) { }
 
@@ -17,6 +19,14 @@ export class ListasComponent implements OnInit {
 
   listaSeleccionada(lista: Lista) {
     console.log(lista);
-    this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
+    if (this.terminada) {
+      this.router.navigateByUrl(`/tabs/tab2/agregar/${lista.id}`);
+    } else {
+      this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
+    }
+  }
+
+  borrarLista(lista: Lista) {
+    this.deseosService.borrarLista(lista);
   }
 }
