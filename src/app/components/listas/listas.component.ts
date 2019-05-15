596f68d0e3d0f0e7a20ba16426d95c3c65381760
Daniel Giraldo <danielgiraldoch@gmail.com>
2019-05-15 13:21:18 -0500

    Aplicacion terminada, editar

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Router } from '@angular/router';
 import { Lista } from 'src/app/models/lista.model';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -10,10 +11,12 @@ import { Lista } from 'src/app/models/lista.model';
 })
 export class ListasComponent implements OnInit {
 
+  @ViewChild(IonList) lista: IonList;
   @Input() terminada = true;
 
   constructor(public deseosService: DeseosService,
-    private router: Router) { }
+    private router: Router,
+    private alertController: AlertController) { }
 
   ngOnInit() { }
 
@@ -24,6 +27,40 @@ export class ListasComponent implements OnInit {
     } else {
       this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
     }
+  }
+
+  async editarLista(lista: Lista) {
+    const alert = await this.alertController.create({
+      header: 'Editar',
+      subHeader: 'Escriba el pendiente para editar ',
+      inputs: [{
+        name: 'titulo',
+        type: 'text',
+        value: lista.titulo,
+        placeholder: 'nombre de la lista '
+      }],
+      buttons: [{
+        text: 'Cancelar', role: 'cancel',
+        handler: () => {
+          console.log('cancelar');
+          this.lista.closeSlidingItems;
+        }
+      },
+      {
+        text: 'Actualizar',
+        handler: (data) => {
+          console.log(data);
+          if (data.titulo.length === 0) {
+            return;
+          }
+          lista.titulo = data.titulo;
+          this.deseosService.guardarStorage();
+          this.lista.closeSlidingItems();
+        }
+      }
+      ]
+    });
+    alert.present();
   }
 
   borrarLista(lista: Lista) {
